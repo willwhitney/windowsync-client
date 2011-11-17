319ec67d79091d2b0649120437d4d1d6b90390e2
Will Whitney <wfwhitney@gmail.com>
2011-11-17 16:30:30 -0500

    removing those things better

--- server_node/sockethandlers.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  var handleEvent;
-  handleEvent = function() {
-    return socket.on('other event', function(data) {
-      return console.log(data);
-    });
-  };
-  exports.handle = handle;
-}).call(this);
